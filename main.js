461aed41a68b39c39bbe126a612256678cb1b0fe
mosala123 <149291901+mosala123@users.noreply.github.com>
2024-02-26 12:56:06 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+// text anmations
+var typed = new Typed("#auto-type", {
+    strings: ["Front-End Dev", "Web Dev"],
+    typeSpeed: 100, // زيادة سرعة الكتابة
+    backSpeed: 100, // زيادة سرعة الرجوع
+    loop: true,
+  });
+
+
+
+ // text anmations second
+var typed = new Typed("#aout-write", {
+  strings: ["Mohamed Ibrahim Salah", " Front-End Developer"],
+  typeSpeed: 100,  
+  backSpeed: 100,  
+  loop: true,
+});
